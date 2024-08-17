7ccb1bc88a9173a27b5da580e1fbf3c3acd8dabe
axelmaya69 <axeldbmaya96@gmail.com>
2024-08-16 18:23:43 -0600

    primer subida

---
+++ src/main/java/com/example/demo/Rutas.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import com.example.demo.models.Libro;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@RestController
+public class Rutas {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+    @GetMapping("/")
+    String miPrimerRuta() {
+        return "Hola mundo desde Springboot";
+    }
+
+    @GetMapping("book/{id}/editorial/{editorial}")
+    String readBook(@PathVariable int id, @PathVariable String editorial) {
+        return "Leyendo el libro: " + id + " con editorial " + editorial;
+
+    }
+
+    @GetMapping("book2/{id}")
+    String readBooks(@PathVariable int id, @RequestParam String gato) {
+        return "Leyendo el libro: " + id + " con editorial " + gato;
+    }
+
+    @PostMapping ("/libre")
+        String guardarLibro(@RequestBody Libro libro) {
+        logger.debug("libro {} editorial {} ",libro.nombre, libro.editorial);
+
+
+
+        return "Libro Gruardado";
+
+    }
+}
---
+++ src/main/java/com/example/demo/models/Libro.java
@@ -0,0 +1,12 @@
+package com.example.demo.models;
+
+public class Libro {
+
+    public String nombre, editorial;
+
+    public Libro(String nombre, String editorial){
+        this.nombre=nombre;
+        this.editorial=editorial;
+
+    }
+}
