4938fbba72f913a4dc4132ee8d1dc2bcbcfc9d5e
axelmaya69 <axeldbmaya96@gmail.com>
2024-08-16 19:44:40 -0600

    nuevas notaciones json

--- src/main/java/com/example/demo/Rutas.java
+++ src/main/java/com/example/demo/Rutas.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
 import com.example.demo.models.Libro;
+import com.example.demo.models.UserData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -14,29 +18,23 @@ public class Rutas {
 
     private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
-    @GetMapping("/")
-    String miPrimerRuta() {
-        return "Hola mundo desde Springboot";
+   @GetMapping("userData")
+    public ResponseEntity<String> getUserData(){
+       return  ResponseEntity
+               .status(HttpStatus.OK)
+               .header("Content-Type","application/json")
+               .body("{\"name\":\"mary\"}") //respuesta jsons mala
+               ;
+   }
+    //mejor forma de devolver jsons
+   @GetMapping("/userDataV2")
+   public Map<String, Map <String,Object>> getUserDataV2(){
+        return Map.of("user", Map.of("name","Mary","age",25));
+   }
+
+    @GetMapping("/userDataV3")
+    public UserData getUserDataV3(){
+    return new UserData("mary",25);
     }
 
-    @GetMapping("book/{id}/editorial/{editorial}")
-    String readBook(@PathVariable int id, @PathVariable String editorial) {
-        return "Leyendo el libro: " + id + " con editorial " + editorial;
-
-    }
-
-    @GetMapping("book2/{id}")
-    String readBooks(@PathVariable int id, @RequestParam String gato) {
-        return "Leyendo el libro: " + id + " con editorial " + gato;
-    }
-
-    @PostMapping ("/libre")
-        String guardarLibro(@RequestBody Libro libro) {
-        logger.debug("libro {} editorial {} ",libro.nombre, libro.editorial);
-
-
-
-        return "Libro Gruardado";
-
-    }
 }
---
+++ src/main/java/com/example/demo/models/UserData.java
@@ -0,0 +1,14 @@
+package com.example.demo.models;
+
+public class UserData {
+
+    public String name;
+    public int age;
+
+public UserData(String name, int age){
+    this.name=name;
+    this.age=age;
+
+
+}
+}
