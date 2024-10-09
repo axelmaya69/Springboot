2dcd4e20ef31611c65d6d3120105cc5fc052ad99
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-08 21:48:11 -0600

    archivos

--- src/main/java/com/example/demo/services/OrderServices.java
+++ src/main/java/com/example/demo/services/OrderServices.java
@@ -1,9 +1,11 @@
 package com.example.demo.services;
 
 import com.example.demo.models.Producto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service //Al implementar la notacion @Service le decimos a Springboot que esta clase es un servicio
 public class OrderServices {
 
 
