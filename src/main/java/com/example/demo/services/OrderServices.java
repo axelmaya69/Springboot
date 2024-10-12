5846d7de14c4b2e5b11376e8aa1f3454514ef40f
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-11 22:40:53 -0600

    beans y polimorfismo

--- src/main/java/com/example/demo/services/OrderServices.java
+++ src/main/java/com/example/demo/services/OrderServices.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service //Al implementar la notacion @Service le decimos a Springboot que esta clase es un servicio
+//Al implementar la notacion @Service le decimos a Springboot que esta clase es un servicio
 //y este va a crear un objeto de esta clase y lo va a reutilizar en todas las clases donde se necesite con inyeccion de
 //dependencias
-public class OrderServices {
+public class OrderServices implements IOrderService {
 
 
     public void saveOrder(List<Producto> productos){
